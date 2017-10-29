8cc9c183b9253fccc92748d1b7d88a1caf475985
Andy Bettisworth <andy@accreu.com>
2017-10-29 00:16:10 -0500

    hookup test and source

---
+++ test/translate_test.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const assert = require('assert')
+const t = require('../lib/translate.js')
+
+describe("#t()", () => {
+  it("should set default locales_path to './config/locales'")
+})
